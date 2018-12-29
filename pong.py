d4f8667ad64ff02182f339a4a03a4fff5ef1f6a9
Dieu <mathieu.caselles@ynov.com>
2018-12-29 21:04:51 +0100

    Taille de balle + facile à changer
    début des essait de mouvement de balle

--- pong.py
+++ pong.py
@@ -1,17 +1,28 @@
 from tkinter import *
 
+
 fenetre = Tk()
 
 # écran de jeu
 hauteur = 1080
 largeur = 1920
 canvas = Canvas(fenetre, width=largeur, height=hauteur, bg='black')
-canvas.pack()
 
 # balle
 
+t_balle = 30
+mouv = -1
 # spawn au milieu de l'écran 
 
-balle = canvas.create_oval(largeur / 2, hauteur / 2, largeur / 2, hauteur / 2, fill='white')
+balle = canvas.create_oval(largeur / 2 - t_balle, hauteur / 2 - t_balle, largeur / 2 + t_balle, hauteur / 2 + t_balle, fill='white')
 
+# faire bouger la balle
+
+direction_x = 0
+direction_y = 0
+def mouvement_balle():
+    pass
+
+    
+canvas.pack()
 fenetre.mainloop()
