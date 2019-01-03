07aa936612ec1d0bfb2471938f180f7a9d037206
Dieu <mathieu.caselles@ynov.com>
2019-01-03 22:08:54 +0100

    gestion raquette
    déplacement + collision

--- pong.py
+++ pong.py
@@ -31,8 +31,7 @@ balle = canvas_jeu.create_oval(largeur / 2 - t_balle, hauteur / 2 - t_balle, lar
 # faire bouger la balle + rebondir
 
 
-
-def jouer():
+def deplacement_balle():
 
     vitesse_x = choice([-1, 1])
     vitesse_y = choice([-1, 1])
@@ -40,24 +39,60 @@ def jouer():
     while True:
 
         canvas_jeu.move(balle, vitesse_x, vitesse_y)
-        position = canvas_jeu.coords(balle) #[gauche, haut, droite, bas] 
+        position_balle = canvas_jeu.coords(balle) #[gauche, haut, droite, bas] 
+        position_raquette_1 = canvas_jeu.coords(raquette_1)
+        position_raquette_2 = canvas_jeu.coords(raquette_2)
 
 
-        if position[2] >= largeur or position[0] <= 0:
+
+        if position_balle[2] >= largeur or position_balle[0] <= 0:
             vitesse_x = -vitesse_x
-        if position[3] >= hauteur or position[1] <= 0:
+        if position_balle[3] >= hauteur or position_balle[1] <= 0:
             vitesse_y = -vitesse_y
+        if position_balle[0] <= position_raquette_1[2] and position_raquette_1[1]<= position_balle[3] and position_raquette_1[3] >= position_balle[1]:
+            vitesse_x = -vitesse_x
+        if position_balle[2] >= position_raquette_2[0] and position_raquette_2[1]<= position_balle[3] and position_raquette_2[3] >= position_balle[1]:
+            vitesse_x = -vitesse_x
 
         fenetre_jeu.update()
         time.sleep(0.01)
 
-        print(position)
-
 
 def lancer_jeu():
     menu_principal.withdraw()
-    jouer()
+    deplacement_balle()
     
+
+# raqutte
+t_raquette = 60
+raquette_1 = canvas_jeu.create_rectangle(15, hauteur / 2 - t_raquette, 30, hauteur / 2 + t_raquette, fill='white')
+raquette_2 = canvas_jeu.create_rectangle(largeur-15, hauteur / 2 - t_raquette, largeur-30, hauteur / 2 + t_raquette, fill='white')
+
+def deplacement_raquette_1(event):
+
+    vitesse = -10
+    canvas_jeu.move(raquette_1, 0, vitesse)
+
+def deplacement_raquette_2(event):
+
+    vitesse = 10
+    canvas_jeu.move(raquette_1, 0, vitesse)
+
+def deplacement_raquette_3(event):
+
+    vitesse = -10
+    canvas_jeu.move(raquette_2, 0, vitesse)
+
+def deplacement_raquette_4(event):
+
+    vitesse = 10
+    canvas_jeu.move(raquette_2, 0, vitesse)
+
+
+fenetre_jeu.bind("<Tab>", deplacement_raquette_1)
+fenetre_jeu.bind("<Shift_L>", deplacement_raquette_2)
+fenetre_jeu.bind("<Up>", deplacement_raquette_3)
+fenetre_jeu.bind("<Down>", deplacement_raquette_4)
 
 # Menu Principal
 
@@ -70,4 +105,5 @@ btn_quitter = Button(menu_principal, text='Quitter', command=menu_principal.dest
 texte_titre.place(relx = 0.5, rely = 0.45, anchor = CENTER)
 btn_jouer.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 btn_quitter.place(relx = 0.5, rely = 0.55, anchor = CENTER)
+
 menu_principal.mainloop()
